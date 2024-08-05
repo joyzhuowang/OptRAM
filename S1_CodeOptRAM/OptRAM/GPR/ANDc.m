b309443b11ea45a52b6e8a2226867c292170202e
Joy Wang <joyzhuowang@gmail.com>
2024-08-05 21:34:24 +0800

    Delete S1_CodeOptRAM/OptRAM directory

--- S1_CodeOptRAM/OptRAM/GPR/ANDc.m
+++
@@ -1,8 +0,0 @@
-function [ output ] = ANDc( a , b )
-output = [0 1];
-if a(1) < b(1);
-    output(1) = a(1);
-else
-    output(1) = b(1);
-end;
-end
--- S1_CodeOptRAM/OptRAM/GPR/ORc.m
+++
@@ -1,7 +0,0 @@
-function [ output ] = ORc( a , b )
-
-output=[0 1];
-output(1) = (a(1)*a(2)+b(1)*b(2))/(a(2)+b(2));
-output(2) = a(2)+b(2);
-end
-
